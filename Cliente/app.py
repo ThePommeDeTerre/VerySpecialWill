1183ee63666fcdc64cc58249550a03b6b120c775
Pedro Batista <p.batista.redocean@gmail.com>
2021-04-23 11:57:19 +0100

    Validação por CSRF

--- Cliente/app.py
+++ Cliente/app.py
@@ -1,18 +1,42 @@
-from flask import Flask, render_template,session
+from flask import Flask, render_template, session, request
 import secrets
 import helpers.SessionHelper as SessionHelper
+import json
+from flask_wtf.csrf import CSRFProtect, \
+    CSRFError  # este nao usem o pip para instalar, mas o gestor de packages do pycharm
 import requests
 
 app = Flask(__name__)
+# Cria a secret key para a sessão e ativa proteção por csrf
 app.config["SECRET_KEY"] = secrets.token_urlsafe(16)
+csrf = CSRFProtect(app)
 
 
 @app.route('/')
 def main():
-    #response = requests.get("http://149.90.108.93:80")
     SessionHelper.start_session(app)
-    return render_template('index.html')
+    return render_template('login.html')
+
+
+# Efetua o login, caso seja inválido ou nao seja post, retorna mensagem de erro
+@app.route('/login', methods=['POST'])
+def login():
+    if request.method == 'POST':
+        params = request.get_json();
+        url = "http://149.90.108.93:80"
+        # response = requests.post(url, data=params)
+        return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
+    else:
+        return json.dumps({'success': False, 'message': 'Ocorreu um erro'}), 200, {'ContentType': 'application/json'}
+
+
+# Como é por ajax, mudo o primeiro parametro para json, e retorno uma mensagem de erro
+@app.errorhandler(CSRFError)
+def handle_csrf_error(e):
+    return json.dumps({'CsrfError': True, 'msg': 'Csrf Inválido'}), 400, {'ContentType': 'application/json'}
 
 
 if __name__ == '__main__':
     app.run()
+
+# response = requests.get("http://149.90.108.93:80")
--- Cliente/static/js/main.js
+++ Cliente/static/js/main.js
@@ -1,28 +1,93 @@
-var IndexHelper = function () {
+// Controlador do index
+const IndexHelper = function () {
 
+    //Funcao para ajudar a ver se está vazio
+    const isEmpty = function (value) {
+        return typeof value == 'string' && !value.trim() || typeof value == 'undefined' || value === null;
+    }
+
+    const init = () => {
+        initTab()
+        initForm()
+
+        // Mete o atual csrf sempre que é efetuada uma mensagem ajax
+        $.ajaxSetup({
+            beforeSend: function (xhr, settings) {
+                const csrf = $('#csrf_token').val();
+                if (!/^(GET|HEAD|OPTIONS|TRACE)$/i.test(settings.type) && !this.crossDomain) {
+                    xhr.setRequestHeader("X-CSRFToken", csrf);
+                }
+            }
+        });
+    }
+
+    //#region formulário
+    //Inicializa o formulário, botões e ações
+    const initForm = () => {
+        $('#btn-login').on('click', login);
+        $('#btn-registo').on('click', login);
+    }
+
+    const login = function () {
+        const user = $('#input-login-username').val();
+        const pass = $('#input-login-password').val();
+
+        if (isEmpty(user) || isEmpty(pass)) {
+            return alert('Por favor preencha todos os campos');
+        }
+
+        //manda o request para o login
+        $.ajax({
+                type: "POST",
+                url: 'login',
+                data: JSON.stringify({user: user, pass: pass}),
+                success: (data, status, xhttp) => {
+                    if (!data.success)
+                        alert(data.msg)
+                    console.log(data, status, xhttp)
+                },
+                error: (data, status, xhttp) => {
+                    let response = data.responseJSON
+                    if (response.CsrfError)
+                        alert(response.msg)
+                },
+                contentType: "application/json",
+                dataType: 'json'
+
+            }
+        )
+
+    }
+
+
+//#endregion
+
+//#region mudar as tabs
     const initTab = () => {
         let tabs = $('.tab-item')
-        $.each(tabs,(i,v) => {
-            $(v).on('click',function(){
+        $.each(tabs, (i, v) => {
+            $(v).on('click', function () {
                 const tabName = $($(this).find('a')[0]).data('tab')
-                changeTab(this,tabName)
+                changeTab(this, tabName)
                 $(this).addClass('active')
             })
         })
     }
 
-    const changeTab = (v,tabName) => {
+    const changeTab = (v, tabName) => {
         $('.tab-content').hide()
         $('.tab-item').removeClass('active')
         $('#' + tabName).show()
         $(this).addClass('active')
     }
+//#endregion
 
     return {
-        initTab: initTab
+        init: init
     }
-}()
+}
+();
 
 $(function () {
-    IndexHelper.initTab()
+    IndexHelper.init()
 })
