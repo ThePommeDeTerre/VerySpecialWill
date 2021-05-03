9b530106785c6cad979e3a2037707912801a8ec8
Pedro Batista <p.batista.redocean@gmail.com>
2021-05-03 21:53:44 +0100

    qrcode 2fa

--- Cliente/app.py
+++ Cliente/app.py
@@ -1,15 +1,22 @@
-from flask import Flask, render_template, session, request, flash
+import time
+
+from flask import Flask, render_template, session, request, flash, send_file
 import secrets
 import helpers.SessionHelper as SessionHelper
 import helpers.CommonHellper as Common
 import static.ServerRoutes as SerRoutes
-import json
+import pyotp
 
 # Este nao usem o pip para instalar, mas o gestor de packages do pycharm
 from flask_wtf.csrf import CSRFProtect, CSRFError
+from flask_qrcode import QRcode
+
 import requests
 
+
+
 app = Flask(__name__)
+qrcode = QRcode(app)
 
 # Cria a secret key para a sessão e ativa proteção por csrf
 app.config["SECRET_KEY"] = secrets.token_urlsafe(16)
@@ -101,6 +108,32 @@ def registo():
 def handle_csrf_error(e):
     return Common.create_response_message(400, True, 'Csrf Inválido', {'CsrfError': True})
 
+
+# region 2fa
+@app.route('/login/2fa', methods=['GET'])
+def login_2fa():
+    try:
+        if request.method == 'POST':
+            params = request.get_json();
+        else:
+
+            # ir buscar secret key do user, por enquanto usar random
+            secret = pyotp.random_base32()
+            uri = pyotp.totp.TOTP(secret).provisioning_uri(name='test@google.com', issuer_name='A Very Special Will')
+            secreturi = uri
+            return render_template('login_2fa.html', secret=secret,secreturi = secreturi)
+    # Caso haja erros de status ou conexão
+    except Exception  as e:
+        print(e)
+        return Common.create_response_message(200, False, 'Ocorreu um erro')
+
+@app.route("/qrcode", methods=["GET"])
+def get_qrcode():
+    # please get /qrcode?data=<qrcode_data>
+    data = request.args.get("data", "")
+    return send_file(qrcode(data, mode="raw"), mimetype="image/png")
+
+# endregion
 
 if __name__ == '__main__':
     app.run()
