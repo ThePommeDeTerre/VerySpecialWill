57aa67ca5897d9c361aa8bb8636eebbbd997ed00
rsbguerra <rsbguerra@gmail.com>
2021-05-06 15:36:22 +0100

    Messing arround with shamir

--- Cliente/helpers/test.py
+++ Cliente/helpers/test.py
@@ -1,7 +1,12 @@
+#!/usr/bin/env python3
+
 import os
+from binascii import hexlify
 from Crypto.Random import get_random_bytes
+from Crypto.Protocol.SecretSharing import Shamir
 from ourAES import ourAES as AES
 from ourHMAC import ourHMAC as HMAC
+
 
 # TODO : Abstract so it accepts files
 with open('text.txt', 'r') as file:
@@ -9,16 +14,27 @@ with open('text.txt', 'r') as file:
 
 key = get_random_bytes(16)
 
-oAES = AES('ECB', key)
-oAES.alterar_modo('CBC')
-
-oHMAC = HMAC('SHA256', key)
-oHMAC.alterar_modo('SHA512')
+oAES = AES('CBC', key)
+oHMAC = HMAC('SHA512', key)
 
 # Cipher and HMAC
 ct_bytes = oAES.cifrar(plaintext, show=True)
 hmac = oHMAC.fazer_hmac(plaintext)
 
+# Criar segredo de shamir para 3 pessoas de forma a precisar
+# de 3 pessoas para reconstruir a chave
+shares = Shamir.split(3, 3, key)
+print("key: %s" % hexlify(key))
+
+for idx, share in shares:
+    print("Index #%d: %s" % (idx, hexlify(share)))
+
+s = shares[0:1] 
+
+
+key = Shamir.combine(s)
+print("key: %s" %  hexlify(key))
+# dividir em métodos
 # Verificar e decifrar
-if oHMAC.verificar_hmac(hmac, plaintext):
-    pt = oAES.decifrar(ct_bytes, show=True)
+#if oHMAC.verificar_hmac(hmac, plaintext):
+#    pt = oAES.decifrar(ct_bytes, show=True)
