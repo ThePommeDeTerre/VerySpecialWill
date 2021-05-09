921582d53b9bd9c1ecc081daf9f98ba862111f5f
Lina Jesus <lina.jesus@ubi.pt>
2021-05-09 15:32:05 +0100

    Redirect requests to auth server

--- Servidor/main.py
+++ Servidor/main.py
@@ -4,10 +4,18 @@ Main Server file
 
 """
 
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
+
+@app.route('/auth/', defaults={'allowed_paths': None})
+@app.route('/auth/<string:allowed_paths>', methods=['POST'])
+def to_auth_server(allowed_paths):
+    if allowed_paths is not None:
+        # 308 - redirect, but preserve the content of your request
+        return redirect("http://localhost:5005/auth/%s" % allowed_paths, code=308)
+    return redirect("http://localhost:5005/auth/", code=301)
 
 @app.route('/')
 def run():
