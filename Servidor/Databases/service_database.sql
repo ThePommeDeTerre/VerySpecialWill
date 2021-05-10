4a0f499d89bb1f531e44f968b564920b00be6476
Lina Jesus <lina.jesus@ubi.pt>
2021-05-10 16:09:20 +0100

    Added service database and flask

---
+++ Servidor/Databases/service_database.sql
@@ -0,0 +1,59 @@
+DROP DATABASE IF EXISTS service_DB;
+
+CREATE DATABASE service_DB;
+USE service_DB;
+
+
+/* ---  USER TABLE ---*/
+
+DROP TABLE IF EXISTS service_user;
+
+CREATE TABLE service_user (
+    service_username VARCHAR(50) PRIMARY KEY,
+    user_pk VARCHAR (150)
+);
+
+
+/* ---  KEY TABLE ---*/
+
+DROP TABLE IF EXISTS share_key 
+
+CREATE TABLE share_key(
+    key_id INT(10) NOT NULL AUTO_INCREMENT
+    value_of_key VARCHAR(150) NOT NULL,
+    PRIMARY KEY key_id
+);
+
+/* ---  WILL TABLE ---*/
+
+DROP TABLE IF EXISTS will;
+
+CREATE TABLE will (
+    will_id INT(10) NOT NULL AUTO_INCREMENT,
+    will_message VARCHAR(200) NOT NULL,
+    will_hmac VARCHAR(150) NOT NULL,
+    will_sign VARCHAR(150) NOT NULL,
+    user_owner VARCHAR(50) NOT NULL,
+    n_min_shares INT(10) NOT NULL,
+    PRIMARY KEY (will_id),
+    FOREIGN KEY (user_owner)
+        REFERENCES service_user (service_username)
+);
+
+
+/* ---  SHARE TABLE ---*/
+
+DROP TABLE IF EXISTS user_share;
+
+CREATE TABLE user_share (
+    username_share VARCHAR(50) NOT NULL,
+    key_id_share INT(10) NOT NULL,
+    will_id_share INT(10) NOT NULL,
+    PRIMARY KEY (username_share, key_id_share),
+    FOREIGN KEY (username)
+        REFERENCES service_user (service_username),
+    FOREIGN KEY (key_id_share)
+        REFERENCES share_key (key_id),
+    FOREIGN KEY (will_id_share)
+        REFERENCES will (will_id)
+);
