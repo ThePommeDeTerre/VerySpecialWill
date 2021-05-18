d84a79eebdfa505d75f643fbba3ebcf834b58516
pedroBatis96 <batped@hotmail.com>
2021-05-18 12:56:51 +0100

    qol changes

--- Cliente/static/ServerRoutes.py
+++ Cliente/static/ServerRoutes.py
@@ -1,6 +1,7 @@
 # IP = "149.90.108.93"
+IP = "149.90.108.93"
 IP = "localhost"
-PORT = "5005"
+PORT = "5000"
 
 SERVER = "http://" + IP + ":" + PORT + "/"
 ROUTES = {
--- Servidor/main.py
+++ Servidor/main.py
@@ -7,6 +7,8 @@ Main Server file
 from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
+#ip = '149.90.108.93'
+ip = 'localhost'
 
 
 @app.route('/auth/', defaults={'allowed_paths': None})
@@ -19,9 +21,9 @@ def to_auth_server(allowed_paths):
 
     if allowed_paths is not None:
         # 308 - Permanent Redirect, but preserve the content of your request
-        return redirect("http://149.90.108.93:5005/auth/%s" % allowed_paths, code=308)
+        return redirect("http://"+ ip + ":5005/auth/%s" % allowed_paths, code=308)
     # 301 - Moved Permanently     
-    return redirect("http://149.90.108.93:5005/auth/", code=301)
+    return redirect("http://"+ ip + ":5005/auth/", code=301)
 
 
 @app.route('/service/', defaults={'allowed_paths': None})
@@ -34,9 +36,9 @@ def to_service_server(allowed_paths):
 
     if allowed_paths is not None:
         # 308 - Permanent Redirect, but preserve the content of your request
-        return redirect("http://149.90.108.93:5007/service/%s" % allowed_paths, code=308)
+        return redirect("http://"+ ip + ":5007/service/%s" % allowed_paths, code=308)
     # 301 - Moved Permanently     
-    return redirect("http://149.90.108.93:5007/service/", code=301)
+    return redirect("http://"+ ip + ":5007/service/", code=301)
 
 
 if __name__ == '__main__':
